d7b5bbf4dd94b094044804098a0dd30c28544177
mjgalarza1 <matias.galarza.unq@gmail.com>
2025-09-01 21:07:32 -0300

    Initial commit

---
+++ src/main/java/com/dapp/futbol_api/FutbolApiApplication.java
@@ -0,0 +1,13 @@
+package com.dapp.futbol_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dapp/futbol_api/FutbolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dapp.futbol_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
